f81e32fd838c395b21a140d562938e5672d7e7a7
BrendonKoz <BrendonKoz@hotmail.com>
2021-08-26 15:46:51 -0400

    Adding work-in-progress changes
    In case I accidentally delete my working folder's changes. Module is completely incomplete.

--- InputfieldBooks.js
+++ InputfieldBooks.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
 
-/*
 	// Taken from InputfieldEvents - not yet modified for this Fieldtype/Inputtype
+    $(document).on('pw-panel-opened', function (eventData, panelNode) {
+    	$(panelNode).find('#isbn')[0].focus();
+    });
 
-	$(document).on("click", ".InputfieldBooksAdd", function(e) {
-		$(this).removeClass('ui-state-active');
-		var $row = $(this).parents(".InputfieldBooks").find("li.BookTemplate");
-		$row.parent('ul').append($row.clone().hide().removeClass('BookTemplate').fadeIn());
-		var id = $(this).attr('id');
-		setTimeout("$('#" + id + "').removeClass('ui-state-active')", 500);
-		return false;
-	});
+    $(document).on('click', '#books_add', function(e) {
+        addBook();
+    });
 
+    function addBook() {
+        isbn = $('#isbn').val();
+        // 1. https://openlibrary.org/api/books?bibkeys=ISBN:9780545582933&format=json&jscmd=data
+        //  - title, cover:small|medium|large, authors[]:name, publish_date (YYYY F)
+        // 2. https://openlibrary.org/api/books?bibkeys=ISBN:9780545582933&format=json&jscmd=data&details=true
+        //  - details->description->value
+        // ... code here ...
+    }
+
+	/*$(document).on("keyup", "#isbn", function(e) {
+		isbnLength = $(this).val().length;
+		if (isbnLength == 10 || isbnLength == 13) {
+			//
+		}
+	});*/
+
+//	$(document).on("click", ".InputfieldBooksAdd", function(e) {
+//		alert('Hi!');
+//		$('input#isbn').focus();
+//		return false;
+//		$(this).removeClass('ui-state-active');
+//		var $row = $(this).parents(".InputfieldBooks").find("li.BookTemplate");
+//		$row.parent('ul').append($row.clone().hide().removeClass('BookTemplate').fadeIn());
+//		var id = $(this).attr('id');
+//		setTimeout("$('#" + id + "').removeClass('ui-state-active')", 500);
+//		return false;
+//	});
+/*
 	$(document).on("click", ".InputfieldBooks a.BookClone", function(e) {
 		var $row   = $(this).parents("li.Book");
 		var $table = $(this).parents("table.InputfieldBooks");
